4892cd71f27584c7c7b7f46ed1c2d28850eccb20
Eric Stout <ericwstout@gmail.com>
2020-11-16 20:03:46 -0600

    Add helmet to express

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import helmet from 'helmet';
 import favicon from 'serve-favicon';
 import path from 'path';
 
@@ -7,11 +8,15 @@ import apiRouter from './routes/api';
 const app = express();
 const port = process.env.PORT || 3000;
 
+// various configs/use
+app.use(helmet());
 app.use(express.static('public'));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
+// api routes
 app.use('/api/v1', apiRouter);
 
+// health check
 app.get('/health', (req: Request, res: Response) => res.sendStatus(200));
 
 app.listen(port, () => {
