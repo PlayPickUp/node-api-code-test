1d771ab42d6a38ba7fe2b70a1e519ce616127bd7
Eric Stout <ericwstout@gmail.com>
2020-11-16 19:37:08 -0600

    Add single prop API call

--- src/ctx/props.ts
+++ src/ctx/props.ts
@@ -4,20 +4,33 @@ import knex from '../util/db';
 // Get props
 export const getProps = async (
   limit: string | undefined = '75',
-  offset: string | undefined = '0'
-): Promise<Prop[] | void> => {
-  try {
-    const props: Prop[] = await knex
-      .select()
-      .from('props')
-      .orderBy('created_at', 'desc')
-      .limit(limit)
-      .offset(offset);
-    if (!props) {
-      throw new Error('Could not get props');
+  offset: string | undefined = '0',
+  id: string
+): Promise<Prop[] | Prop | void> => {
+  if (id) {
+    try {
+      const prop: Prop = await knex.select().from('props').where({ id });
+      if (!prop) {
+        throw new Error('Could not get props');
+      }
+      return prop;
+    } catch (err) {
+      console.error(err);
     }
-    return props;
-  } catch (err) {
-    console.error(err);
+  } else {
+    try {
+      const props: Prop[] = await knex
+        .select()
+        .from('props')
+        .orderBy('created_at', 'desc')
+        .limit(limit)
+        .offset(offset);
+      if (!props) {
+        throw new Error('Could not get props');
+      }
+      return props;
+    } catch (err) {
+      console.error(err);
+    }
   }
 };
