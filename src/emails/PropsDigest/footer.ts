399de6135a8c43eed393517d07156de9a754bf6f
Eric Stout <ericwstout@gmail.com>
2020-12-04 11:27:00 -0600

    Update email styles, add send to as ENV

--- src/emails/PropsDigest/footer.ts
+++ src/emails/PropsDigest/footer.ts
@@ -1,6 +1,13 @@
 export const footer = `</div><div></div></div></td>
       </tr>
     </tbody>
+  </table><table class="module" role="module" data-type="spacer" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="53541cf8-e85b-4d97-9769-ad0507c1bbd5">
+    <tbody>
+      <tr>
+        <td style="padding:0px 0px 30px 0px;" role="module-content" bgcolor="">
+        </td>
+      </tr>
+    </tbody>
   </table><table border="0" cellpadding="0" cellspacing="0" class="module" data-role="module-button" data-type="button" role="module" style="table-layout:fixed;" width="100%" data-muid="4676ce44-444f-45b4-b534-50eecc5c34d0">
       <tbody>
         <tr>
@@ -17,7 +24,18 @@ export const footer = `</div><div></div></div></td>
           </td>
         </tr>
       </tbody>
-    </table><div data-role="module-unsubscribe" class="module" role="module" data-type="unsubscribe" style="color:#444444; font-size:12px; line-height:20px; padding:16px 16px 16px 16px; text-align:Center;" data-muid="4e838cf3-9892-4a6d-94d6-170e474d21e5"><div class="Unsubscribe--addressLine"></div><p style="font-size:12px; line-height:20px;"><a target="_blank" class="Unsubscribe--unsubscribeLink zzzzzzz" href="{{{unsubscribe}}}" style="">Unsubscribe</a> - <a href="{{{unsubscribe_preferences}}}" target="_blank" class="Unsubscribe--unsubscribePreferences" style="">Unsubscribe Preferences</a></p></div></td>
+    </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="6c219ac5-cd1e-47f7-8365-e53f0e0f827a" data-mc-module-version="2019-10-22">
+    <tbody>
+      <tr>
+        <td style="padding:18px 0px 18px 0px; line-height:14px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><div style="font-family: inherit; text-align: center"><br></div>
+<div style="font-family: inherit; text-align: center"><br></div>
+<div style="font-family: inherit; text-align: center"><span style="font-family: arial, helvetica, sans-serif; font-size: 12px; color: #8a8a8a"><strong>⚡ PickUp</strong></span></div>
+<div style="font-family: inherit; text-align: center"><span style="font-family: arial, helvetica, sans-serif; font-size: 12px; color: #8a8a8a">&nbsp;</span><span style="font-size: 12px; color: #8a8a8a">365 Bond Street A106</span></div>
+<div style="font-family: inherit; text-align: center"><span style="font-size: 12px; color: #8a8a8a">Brooklyn, NY 11231 USA</span></div>
+<div style="font-family: inherit; text-align: center"><br></div><div></div></div></td>
+      </tr>
+    </tbody>
+  </table><div data-role="module-unsubscribe" class="module" role="module" data-type="unsubscribe" style="color:#444444; font-size:12px; line-height:20px; padding:16px 16px 16px 16px; text-align:Center;" data-muid="4e838cf3-9892-4a6d-94d6-170e474d21e5"><div class="Unsubscribe--addressLine"></div><p style="font-size:12px; line-height:20px;"><a target="_blank" class="Unsubscribe--unsubscribeLink zzzzzzz" href="{{{unsubscribe}}}" style="">Unsubscribe</a></p></div></td>
                                       </tr>
                                     </table>
                                     <!--[if mso]>
