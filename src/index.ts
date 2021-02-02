73194f9a35c0ad7433b3c02033ed5de8af23e499
David Yarbrough <david.yarbrough@playpickup.com>
2021-02-01 23:17:52 -0600

    clearing out commented code

--- src/index.ts
+++ src/index.ts
@@ -49,28 +49,3 @@ app.listen(port, () => {
   console.log(`PickUp API listening on ${port}`);
 });
 
-/**
- * Webpack HMR Activation
- */
-//
-// type ModuleId = string | number;
-//
-// interface WebpackHotModule {
-//   hot?: {
-//     data: any;
-//     accept(
-//         dependencies: string[],
-//         callback?: (updatedDependencies: ModuleId[]) => void,
-//     ): void;
-//     accept(dependency: string, callback?: () => void): void;
-//     accept(errHandler?: (err: Error) => void): void;
-//     dispose(callback: (data: any) => void): void;
-//   };
-// }
-//
-// declare const module: WebpackHotModule;
-//
-// if (module.hot) {
-//   module.hot.accept();
-//   module.hot.dispose(() => server.close());
-// }
