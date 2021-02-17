69d81d6353f2a85502c5fdab422b30a9c0e65fa1
Eric Stout <ericwstout@gmail.com>
2021-02-17 16:38:11 -0600

    Update cors whitelist

--- src/constants/corsWhitelist.ts
+++ src/constants/corsWhitelist.ts
@@ -1,14 +1,11 @@
 export const corsWhitelistDev = [
   'http://localhost:3000',
-  'https://www.playpickup.com',
-  'https://p.pckp.io',
   'https://staging.playpickup.com',
   'https://s.pckp.io',
+  'https://pickup-pr-679.herokuapp.com/',
 ];
 
 export const corsWhitelistProd = [
   'https://www.playpickup.com',
   'https://p.pckp.io',
-  'https://staging.playpickup.com',
-  'https://s.pckp.io',
 ];
