726624eaee8b7b5bcb0227f203b66886077078de
Eric Stout <ericwstout@gmail.com>
2021-03-11 16:04:05 -0600

    Fix publisher meta connections (#45)

--- src/services/posts.service.ts
+++ src/services/posts.service.ts
@@ -16,6 +16,12 @@ export interface GetPosts {
   ): Promise<Post | Post[] | void>;
 }
 
+export interface PublisherMeta {
+  id: string | number;
+  source_url: string;
+  logo: string;
+}
+
 // get posts
 export const getPosts: GetPosts = async (
   limit = 25,
@@ -76,44 +82,54 @@ export const createPost = async (body: PostCreate): Promise<string | void> => {
     publisher_name,
   } = body;
   try {
-    let publisherMeta;
-
-    if (publisher_id) {
-      publisherMeta = await knex('publishers')
+    const getPublisherMeta = async (): Promise<PublisherMeta | null> => {
+      if (!publisher_id) {
+        return null;
+      }
+      const meta = await knex('publishers')
         .select('id', 'source_url', 'logo')
         .where({ id: publisher_id })
         .catch((err: string) => {
           throw new Error(err);
         });
-    }
+      return meta && meta[0];
+    };
 
-    const post = await knex('posts')
-      .insert({
-        post_title,
-        league: JSON.stringify({ leagues: league }),
-        article_url,
-        headline,
-        excerpt,
-        featured_img,
-        prop_id,
-        publisher_name,
-        author_id,
-        publisher_id,
-        publisher_logo:
-          (publisherMeta &&
-            publisherMeta.length > 0 &&
-            publisherMeta[0]?.publisher_logo) ||
-          publisher_logo,
-        publisher_source_url:
-          (publisherMeta &&
-            publisherMeta.length > 0 &&
-            publisherMeta[0]?.publisher_source_url) ||
-          publisher_source_url,
+    const post = await getPublisherMeta()
+      .then(async (meta) => {
+        if (!meta) {
+          console.info(
+            `No publisher meta found, skipping publisher population for ${article_url}, prop id: ${prop_id}, publisher: ${publisher_name}`
+          );
+        }
+        const post = await knex('posts')
+          .insert({
+            post_title,
+            league: JSON.stringify({ leagues: league }),
+            article_url,
+            headline,
+            excerpt,
+            featured_img,
+            prop_id,
+            publisher_name,
+            author_id,
+            publisher_id,
+            publisher_logo: meta?.logo || publisher_logo || '',
+            publisher_source_url:
+              meta?.source_url || publisher_source_url || '',
+          })
+          .catch((err: string) => {
+            throw new Error(err);
+          });
+        if (post.name === 'Error') {
+          throw new Error(post.message);
+        }
+        return post;
       })
-      .catch((err: string) => new Error(err));
-    if (post.name === 'Error') {
-      throw new Error(post.message);
-    }
+      .then((post) => post)
+      .catch((err) => {
+        throw new Error(err);
+      });
     return post;
   } catch (err) {
     console.error(err);
