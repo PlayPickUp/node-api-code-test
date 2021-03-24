cd4e601b629c3533cf78a1d00ff1b6688672493f
Eric Stout <ericwstout@gmail.com>
2021-03-23 19:20:31 -0500

    Update payload destructuring for post patches

--- src/services/events.service.ts
+++ src/services/events.service.ts
@@ -1,22 +1,20 @@
 import warehouse_db_knex from '../util/db';
 import { LogEvent } from '../models/events.model';
 
-export const createNewEvent = async (
-    event: LogEvent
-): Promise<void> => {
-    try {
-        await warehouse_db_knex("events")
-            .insert({
-                name: event.name,
-                event_timestamp: event.event_timestamp || new Date().toISOString(),
-                metadata: event.metadata
-            })
-            .catch((err: string) => {
-                console.error(err)
-            });
-    } catch (err) {
+export const createNewEvent = async (event: LogEvent): Promise<void> => {
+  try {
+    await warehouse_db_knex('events')
+      .insert({
+        name: event.name,
+        event_timestamp: event.event_timestamp || new Date().toISOString(),
+        metadata: event.metadata,
+      })
+      .catch((err: string) => {
         console.error(err);
-        return;
-    }
+      });
+  } catch (err) {
+    console.error(err);
     return;
+  }
+  return;
 };
